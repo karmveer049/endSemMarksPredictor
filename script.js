28c141018c9c3b46b9e732b127ef9c5600523dbe
Karmveer <23bee050@nith.ac.in>
2026-02-15 19:48:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let chart;
-
 async function predict(){
 
   const resultBox = document.getElementById("result");
@@ -23,7 +21,7 @@ async function predict(){
     return;
   }
 
-  resultBox.innerText = "Waking AI server... (first load may take ~40s)";
+  resultBox.innerText = "Waking AI server... (first load ~40s)";
 
   try{
 
@@ -41,57 +39,6 @@ async function predict(){
   }catch(err){
 
     resultBox.innerText =
-      "Server waking up... click again in 20s";
+      "Server waking... click again in 20s";
   }
-}
-
-
-  resultBox.innerText = "Connecting to AI server...";
-
-  try{
-
-    const res = await fetch("https://endsem-api.onrender.com/predict",{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify({
-        attendance: attendance,
-        midsem: midsem,
-        iq: iq,
-        study: study,
-        attentive: attentive
-      })
-    });
-
-    const data = await res.json();
-
-    resultBox.innerText =
-      "Predicted End-Sem Marks: " + data.prediction.toFixed(2) + "%";
-
-    updateChart([attendance, midsem, iq, study, attentive]);
-
-  }catch(err){
-
-    resultBox.innerText =
-      "Server waking up… wait 30s and click again";
-  }
-}
-
-function updateChart(values){
-
-  if(chart) chart.destroy();
-
-  chart = new Chart(document.getElementById("chart"),{
-    type:"bar",
-    data:{
-      labels:["Attendance","MidSem","IQ","Study","Attention"],
-      datasets:[{
-        data:values
-      }]
-    },
-    options:{
-      plugins:{legend:{display:false}}
-    }
-  });
 }
